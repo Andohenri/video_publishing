bb74e61ae15df1e293bb84002a27d8c494ac9986
Andohenri <andohenrirazafinatoandro@gmail.com>
2024-07-07 18:53:00 +0300

    config dtructure de dossier et la tailwind css

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View className="flex-1 items-cemter justify-center bg-white text-primary">
+      <Text>Hello DjangoBona</Text>
+    </View>
+  )
+}
+
+export default App
